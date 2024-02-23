87ea1c8dbdd1dc30b1db5df97ae5085e70c0f0d1
Arina Bezdushnaya <ariiinaaa@gmail.com>
2024-02-23 19:13:49 +0300

    + add commands

--- bot.ts
+++ bot.ts
@@ -6,13 +6,38 @@ const BOT_TOKEN = process.env.BOT_TOKEN as string;
 //Create a new bot
 const bot = new Bot(BOT_TOKEN);
 
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-bot.on("message", (ctx) => {
-    const message = ctx.message; // the message object
-    console.log(message);
+// Список команд
+// play - Начать игру
+// goal - Цель игры
+// objectives - Задачи игры
+// rules - Как играть
 
-    ctx.reply(`Привет, ${ctx.from.first_name}!`);
+bot.command("start", (ctx) =>
+  ctx.reply(`Привет, ${ctx.from?.first_name || "друг"}!`)
+);
+
+bot.command("goal", (ctx) => {
+  const goalTitle = "Цель игры";
+  const goalText = "Восстановить картину преступления";
+
+  ctx.reply(`${goalTitle} - ${goalText}.`);
 });
+
+bot.command("objectives", (ctx) => {
+  const objectivesTitle = "Задачи игры";
+  const objectivesText = "1. Прочитайте запись звонка в службу спасения. 2. ";
+
+  ctx.reply(`${objectivesTitle} - ${objectivesText}.`);
+});
+
+// bot.on("message", (ctx) => {
+//   const message = ctx.message; // the message object
+//   console.log(message);
+
+//   ctx.reply(`Привет, ${ctx.from.first_name}!`);
+// });
+
+// bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
 //Start the Bot
 bot.start();
